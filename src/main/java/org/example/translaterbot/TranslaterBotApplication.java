48a25bf2640b3d183c19b53cc31b117ab3fcd310
egor <Egor78396@gmail.com>
2024-10-16 16:46:01 +0300

    first commit

---
+++ src/main/java/org/example/translaterbot/TranslaterBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.translaterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslaterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslaterBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/translaterbot/TranslaterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.translaterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslaterBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
